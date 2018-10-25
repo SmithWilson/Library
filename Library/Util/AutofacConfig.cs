92dbd31e26a3839b147604f571e9973e10a4b1b5
Smith Wilson <moskalev99@mail.ru>
2018-10-25 13:59:03 +0300

    Added IoC Autofac

---
+++ Library/Util/AutofacConfig.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Util
+{
+    public class AutofacConfig
+    {
+        /// <summary>
+        /// Настройка IoC Autofac
+        /// </summary>
+        public static void ConfigureContainer()
+        {
+            //Создание контейнера.
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            //Регистрация зависимостей
+
+
+            //Внедрение зависимостей в контейнер
+            var container = builder.Build();
+
+            //Установка сапоставление зависимостей
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
