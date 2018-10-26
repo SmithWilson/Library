8bbc1313d4485d51d9fde0f79efbc2245b25a452
Smith Wilson <moskalev99@mail.ru>
2018-10-26 15:41:10 +0300

    Fix model - book

---
+++ Entity/Migrations/201810261025436_ValidationBook.cs
@@ -0,0 +1,22 @@
+namespace Entity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ValidationBook : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Books", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Books", "Genre", c => c.String(nullable: false));
+            AlterColumn("dbo.Books", "Author", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Books", "Author", c => c.String());
+            AlterColumn("dbo.Books", "Genre", c => c.String());
+            AlterColumn("dbo.Books", "Name", c => c.String());
+        }
+    }
+}
--- Entity/Models/Book.cs
+++ Entity/Models/Book.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +10,26 @@ namespace Entity.Models
 {
     public class Book
     {
+        [Required]
+        [RegularExpression(@"[0-9]+")]
         public int Id { get; set; }
 
+        [DisplayName("Название")]
+        [Required(ErrorMessage = "Не указано название книги")]
         public string Name { get; set; }
 
+        [DisplayName("Дата выхода")]
+        [Required(ErrorMessage = "Не указана дата выхода")]
         public DateTimeOffset ReleaseDate { get; set; }
 
+        [DisplayName("Жанр")]
+        [Required(ErrorMessage = "Не указан жанр книги")]
+        [RegularExpression(@"[a-zA-Zа-яА-Я\s]+", ErrorMessage = "Жанр должен состоять из букв латиницы или кириллицы")]
         public string Genre { get; set; }
 
+        [DisplayName("Автор")]
+        [Required(ErrorMessage = "Не указан автор книги")]
+        [RegularExpression(@"[a-zA-Zа-яА-Я\s]+", ErrorMessage = "Жанр должен состоять из букв латиницы или кириллицы")]
         public string Author { get; set; }
     }
 }
